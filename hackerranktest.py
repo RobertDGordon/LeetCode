d08c5bbd78899f71e9313bd8d393de9d08c95f56
Robert Gordon <Komeuppance@gmail.com>
2020-06-28 20:51:30 -0700

    Added threeNumberSum

--- hackerranktest.py
+++ hackerranktest.py
@@ -61,3 +61,46 @@ def balancedBrackets(string):
 
     #time complexity: O(n) - looping n times, n for each character in string
     #space complexity: O(1) - using counters to track each type of bracket, no arrays/lists
+
+def threeNumberSum(arr, target):
+    #sort array
+    arr.sort()
+    print("input", arr, target)
+    #create array for result
+    result = []
+    #create set using arr for fast number searching
+    numSet = set(arr)
+    #create flagged dict
+    flagged = {}
+
+    #edge cases:
+    if len(arr) < 3:
+        return result #if length is less than three, no matches possible, return empty array
+    if len(arr) == 3:
+        return [arr] #if length is three, no need to calculate, return three numbers in array
+    
+    if target == 0:
+        for index, num in enumerate(arr): #loop over arr, using enumerate to have index count
+            for num2 in arr[arr[index]:]: #loop over arr starting at current index of outer loop, set to num2
+                #check if outer loop number(positive value) minus inner loop value is in the array(numSet), means the equation has a match
+                #also check if num2 has not been added to flagged
+                if abs(num) - num2 in numSet and num2 not in flagged.values():
+                    third = abs(num) - num2 #third number in equation
+                    flagged.update({abs(num): num2}) #flag both numbers, setting outer loop as key, and inner loop as value
+                    newArr = [num, num2, third]
+                    #print('flagging', third, 'num2', num2)
+                    print('found', abs(num), '-', num2, '=', third)
+                    result.append(newArr) #add the matched equation to the result as a 2d dimensional array
+                return result
+
+    #simple case, loop over each number and calculate
+    for num in arr:
+        #check if target divided by two minus current number is in numSet
+        #also check that current num has already not been flagged
+        #target must be divided by two because it will be the third number in the compliment
+        if target/2 - num in numSet and num not in flagged.values():
+            second_num = target/2 - num #second_number in compliment
+            flagged.update({num:int(second_num)}) #add numbers to flagged dict
+            result.append([num, int(second_num), int(target/2)]) #add to result
+    print("flagged", flagged)
+    return result
