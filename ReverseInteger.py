4c7d2fcddcc0212f7e2054d076c424f1ed684d51
Robert Gordon <Komeuppance@gmail.com>
2020-09-01 20:19:53 -0700

    ReverseInteger solved

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -1,4 +1,14 @@
 def reverse(self, x: int) -> int:
-    rev = str(x)
-    rev = rev[::-1]
+    rev = str(x)    
+    neg = False
+    if rev[0] == '-':
+        neg = True
+        rev = rev[1:]
+    rev = int(rev[::-1])
+    if rev < -2**31:
+        return 0
+    if rev > 2**31 - 1:
+        return 0
+    if neg == True:
+        rev = rev * -1
     return rev
--- luckynums.py
+++ luckynums.py
@@ -1,25 +1,30 @@
-class Solution:
-    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        #extract each array as a row
-        #loop over row_array and find the min
-        #
- 
-        row_arr = []
-        min_num = None
-        for row in matrix:
-            row_arr = row
-            for row_something in row_arr:
-                next_num = row_arr[+1]
-                if row_something < next_num:
-                    min_num = row_something
+def luckyNumbers (matrix):
+    #extract each array as a row
+    #loop over row_array and find the min
+    #
 
-        col_num = []
-        for i in range(len(row_arr)):
-            col_num = row_arr
-            if min_num in col_num:
-                return [min_num]
+    row_arr = []
+    min_num = None
+    min_nums = []
+    for row in matrix:
+        min_row_num = None
+        row_arr = row
+        for row_something in row_arr:
+            next_num = row_arr[+1]
+            if min_row_num is None:
+                min_row_num = row_something
+            if row_something > min_row_num:
+                 
+            if row_something < next_num:
+                min_num = row_something
+                print(min_num)
+    col_num = []
+    for i in range(len(row_arr)):
+        col_num = row_arr
+        if min_num in col_num:
+            return [min_num]
         
-        
+print(luckyNumbers([[7,8],[1,2]]))
         
 #         3  7  8 min = 3
 #         9  11 13 min = 9
