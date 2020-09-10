1892e320b6bf9c0a8eca40451bc950bead548304
Robert Gordon <Komeuppance@gmail.com>
2020-09-09 21:46:54 -0700

    Plan

---
+++ RomanToInteger.py
@@ -0,0 +1,12 @@
+# Loop over string until it reaches I
+# Seperate at I
+# Calculate first half (pre I) of the string
+# Calculate second hald (post I)
+# Add together and return
+
+
+
+def romanToInt(s):
+    pass
+
+romanToInt()
