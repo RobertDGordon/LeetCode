7759ede21279ca6b991b4a505a3d0c1543aa3391
Robert Gordon <Komeuppance@gmail.com>
2020-09-09 22:10:35 -0700

    Second Pass

--- RomanToInteger.py
+++ RomanToInteger.py
@@ -1,23 +1,35 @@
-# Loop over string until it reaches I
-# Seperate at I
-# Create marker, set true at I
-# Calculate first half (pre I) of the string
-# Calculate second hald (post I)
-# Add together and return
+#Loop over string with enumerate
+#If C check if next is D or M, subtract by add 400 or 900
+#If X check if next is L or C, add 40 or 90
+#If I check if next is V or X, add 4 or 9
+#Add value to result
+
 
 
 
 def romanToInt(s):
-    firsthalf = ""
-    secondhalf = ""
-    marker = False
-    for char in s:
-        if char == "I" and marker == False:
-            marker = True
-        if marker == False:
-            firsthalf += char
-        if marker == True:
-            secondhalf += char
-    print(firsthalf, secondhalf)
+    # firsthalf = ""
+    # secondhalf = ""
+    # marker = False
+    result = int
+    # for char in s:
+    #     if char == "I" and marker == False:
+    #         marker = True
+    #     if marker == False:
+    #         firsthalf += char
+    #     if marker == True:
+    #         secondhalf += char
+    for char, index in enumerate(s):
+        if char == "M":
+            result += 1000
+        if char == "D":
+            result += 500
+        if char == "C":
+            result += 100
+        if char == "L":
+            result += 50
+        if char == "X":
+            result +=10
+    # print(firsthalf, secondhalf)
 
-romanToInt("MCMIV")
+romanToInt("LVIII")
