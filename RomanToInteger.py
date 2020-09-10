52581bcade89309abd951868b42bdd2c32c90f4d
Robert Gordon <Komeuppance@gmail.com>
2020-09-09 21:59:59 -0700

    First pass

--- RomanToInteger.py
+++ RomanToInteger.py
@@ -1,5 +1,6 @@
 # Loop over string until it reaches I
 # Seperate at I
+# Create marker, set true at I
 # Calculate first half (pre I) of the string
 # Calculate second hald (post I)
 # Add together and return
@@ -7,6 +8,16 @@
 
 
 def romanToInt(s):
-    pass
+    firsthalf = ""
+    secondhalf = ""
+    marker = False
+    for char in s:
+        if char == "I" and marker == False:
+            marker = True
+        if marker == False:
+            firsthalf += char
+        if marker == True:
+            secondhalf += char
+    print(firsthalf, secondhalf)
 
-romanToInt()
+romanToInt("MCMIV")
