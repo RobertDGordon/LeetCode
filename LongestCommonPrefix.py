344ba541e1039ad60f297366e0f9cedec18c6549
Robert Gordon <Komeuppance@gmail.com>
2020-09-14 21:21:38 -0700

    Still working on it...

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -3,16 +3,19 @@
 
 def longestCommonPrefix(strs):
     strs.sort()
+    strs.sort(reverse=True, key = lambda x:len(x))
+    print(strs)
     prefix = ""
-    for i in range(len(strs)):
-        first = strs[i][0]
-        print(first)
-        for index, ch in enumerate(strs[i]):
+    first = strs[0][0]
+    for current_string in range(len(strs)-1):
+        if strs[current_string][0] == strs[current_string + 1][0]:
+            print("match:", strs[current_string][0])
+        for index, char in enumerate(strs[current_string]):
             # print(index, ch)
             pass
 
     return prefix
 
-strs=["flower","flow","flight"]
+strs=["flower","racecar","flow","flight","fireys"]
 
 longestCommonPrefix(strs)
