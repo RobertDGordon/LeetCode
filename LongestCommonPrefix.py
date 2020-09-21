861a77b0a080f88d223e470455e79ee3470bbb0e
Robert Gordon <Komeuppance@gmail.com>
2020-09-20 17:54:20 -0700

    Add psuedocode

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -2,20 +2,30 @@
 #
 
 def longestCommonPrefix(strs):
+    if len(strs) < 1:
+        return ""
     strs.sort()
-    strs.sort(reverse=True, key = lambda x:len(x))
-    print(strs)
+    strs.sort(key = lambda x:len(x))
+    # print(strs)
     prefix = ""
     first = strs[0][0]
     for current_string in range(len(strs)-1):
         if strs[current_string][0] == strs[current_string + 1][0]:
-            print("match:", strs[current_string][0])
-        for index, char in enumerate(strs[current_string]):
-            # print(index, ch)
-            pass
+            # print("match:", strs[current_string][0])
+            prefix = strs[current_string][0]
+            for second_index in range(len(strs[current_string])-1):
+                # print("\nnext_char:", strs[current_string][second_index + 1])
+                next_char = strs[current_string][second_index + 1]
+                if next_char == strs[current_string + 1][second_index + 1]:
+                    # print("second letter match:", strs[current_string + 1][second_index + 1])
+                    prefix += strs[current_string + 1][second_index + 1]
+                else:
+                    break
+        else:
+            current_string += 1
 
     return prefix
 
 strs=["flower","racecar","flow","flight","fireys"]
 
-longestCommonPrefix(strs)
+print(longestCommonPrefix(strs))
---
+++ ValidParentheses.py
@@ -0,0 +1,9 @@
+#return true if all brackets are closed, in the correct order
+#return false if uneven number, or out of order
+#edge cases?
+
+#Loop over string
+#Break loop if closed bracket is found before open
+#If open bracket is found, add to specific bracket counter type
+#If closed bracket is found, add to specific closed bracket counter
+#check that open brackets are equal to closed brackets, return true, else false
