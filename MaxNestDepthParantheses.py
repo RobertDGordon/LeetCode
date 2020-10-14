584a9671613f7bed7b07bd3c967a7ce929251068
Robert Gordon <Komeuppance@gmail.com>
2020-10-13 19:54:11 -0700

    Added plan

---
+++ MaxNestDepthParantheses.py
@@ -0,0 +1,8 @@
+#loop over string
+#count number of open
+#count number of closed
+#check if open > closed
+    #increment nest counter
+#check if open = closed
+    #if not return 0
+    #if true return open + nest
