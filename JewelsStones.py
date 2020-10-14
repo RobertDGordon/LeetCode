10bd7a71fedfacdab3a07a3166f8a45b3301f961
Robert Gordon <Komeuppance@gmail.com>
2020-10-13 19:38:51 -0700

    Added initial loop

---
+++ JewelsStones.py
@@ -0,0 +1,14 @@
+#each character in the string is a value
+#parse the jewels string into a dict
+#loop over stones string and increment counter on match
+
+def numJewelsInStones(J, S):
+    count = 0
+    jewels = []
+    for jewel in J:
+        jewels.append(jewel)
+    print(jewels)
+
+J = "abced"
+
+numJewelsInStones(J, '')
