cb0febf57316b886f9540e4cb0ebdfdaf79afd53
Robert Gordon <Komeuppance@gmail.com>
2020-12-07 15:22:10 -0800

    First pass success

--- DesignParkingSystem.js
+++ DesignParkingSystem.js
@@ -2,33 +2,33 @@
 //addCar function returns true or false based on carType
 
 var ParkingSystem = function (big, medium, small) {
-    big = big;
-    medium = medium;
-    small = small;
+    this.big = big;
+    this.medium = medium;
+    this.small = small;
 };
 
 ParkingSystem.prototype.addCar = function (carType) {
     switch(carType){
         case 1:
             // check if there is space available for this car type
-            if (ParkingSystem.big > 0){
-                ParkingSystem.big --
+            if (this.big > 0){
+                this.big --
                 return true     
             } else {
                 return false
             }
             break;
         case 2:
-            if (ParkingSystem.big > 0){
-                ParkingSystem.big --
+            if (this.medium > 0){
+                this.medium --
                 return true     
             } else {
                 return false
             }
             break;
         case 3:
-            if (ParkingSystem.big > 0){
-                ParkingSystem.big --
+            if (this.small > 0){
+                this.small --
                 return true     
             } else {
                 return false
@@ -39,4 +39,5 @@ ParkingSystem.prototype.addCar = function (carType) {
 }
 
 var obj = new ParkingSystem(1, 1, 0)
+console.log(obj.big)
 
