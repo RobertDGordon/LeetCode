e8b805f4007086a8c59ee3bfe14481c0a38e8c7a
Robert Gordon <Komeuppance@gmail.com>
2020-12-07 14:54:43 -0800

    First pass success

---
+++ GoalParser.js
@@ -0,0 +1,26 @@
+// Iterate over the string
+// Use if/else block to check for keywords
+// advance pointer if () or (al)
+
+const interpret = function (command) {
+    response = "";
+
+    for (var i = 0; i < command.length; i++){
+        // console.log(command[i])
+        if (command[i] === "G"){
+            response += "G"
+        } else if (command[i] === "("){
+            if (command[i + 1] === ")"){
+                response += "o";
+                //add pointer advance here
+            } else if (command[i + 1] === "a"){
+                response += "al"
+                //add pointer advance here
+            }
+        }
+    }
+    console.log(response)
+    
+}
+
+interpret("G()(al)")
