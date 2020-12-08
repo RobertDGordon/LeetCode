7dba325c176168ca88418c295217b6d6fe60d48a
Robert Gordon <Komeuppance@gmail.com>
2020-12-07 18:38:04 -0800

    Added python version

---
+++ NumberOfDaysBetween.py
@@ -0,0 +1,10 @@
+def days(date):
+    year = int(date[0:4])
+    month = int(date[5:7])
+    days = int(date[8:10])
+    print(days)
+
+days("2020-12-23")
+
+def daysBetweenDates(date1, date2):
+    pass
