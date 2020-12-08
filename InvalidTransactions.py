bffc29a896b2abcf4702585587ca92c7731b6c33
Robert Gordon <Komeuppance@gmail.com>
2020-12-07 23:45:37 -0800

    Added not in check

--- InvalidTransactions.py
+++ InvalidTransactions.py
@@ -23,12 +23,14 @@ def invalidTransactions(transactions):
         for idx_2, name_2 in enumerate(name):
             if name[idx_1] == name[idx_2]:
                 if amount[idx_1] > 1000:
-                    flagged.append(transactions[idx_1])
-                    print('Flagged - amount!', transactions[idx_1])
+                    if transactions[idx_1] not in flagged:
+                        flagged.append(transactions[idx_1])
+                        print('Flagged - amount!', transactions[idx_1])
                 elif city[idx_1] != city[idx_2] and (time[idx_2] - time[idx_1] < 60):
-                    flagged.append(transactions[idx_1])
-                    print('Flagged - time!', transactions[idx_1])
+                    if transactions[idx_1] not in flagged:
+                        flagged.append(transactions[idx_1])
+                        print('Flagged - time!', transactions[idx_1])
     print(flagged)
 
 
-invalidTransactions(["alice,20,800,mtv", "alice,50,100,beijing"])
+invalidTransactions(["alice,20,800,mtv", "alice,50,1200,mtv"])
